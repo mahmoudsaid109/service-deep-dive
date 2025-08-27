93e89ebd65009ac0db026c3ac8111c748cc57c18
si3aa <mahmoud22said22@gmail.com>
2025-08-27 12:28:20 +0300

    generate project files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TasksComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('service-deep-dive');
-}
+export class App {}
