d99db1dfcf8286f3c4641fe3a7f4f7fdf6d5e943
si3aa <mahmoud22said22@gmail.com>
2025-08-28 14:38:34 +0300

    Inject tasksServices in tasksComponent

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TasksServices } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   imports: [NewTaskComponent, TasksListComponent],
+  // providers:[TasksServices]
 })
 export class TasksComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { TasksServices } from './app/tasks/tasks.service';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, appConfig,
+//   {
+//   providers:[TasksServices]
+// }
+)
   .catch((err) => console.error(err));
